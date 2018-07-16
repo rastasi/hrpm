98be4b1343d9535d6759783a53919a58d23dcc55
Tasnádi Zsolt <tasi@syrakuza.hu>
2018-07-16 20:25:36 +0200

    dashboad display pimp

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,6 +5,39 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
     columns do
       column do
+        panel 'Active projects' do
+          table do
+            thead do
+              th do
+                td 'Name'
+                td 'Begin date'
+                td 'End date'
+                td 'Project manager'
+                td 'Members'
+                td 'PM URL'
+                td 'Status'
+              end
+            end
+            tbody do
+              Project.active.each do |project|
+                tr do
+                  td
+                  td link_to project.name, admin_project_path(project)
+                  td l(project.begin_date)
+                  td l(project.end_date)                   
+                  td project.project_manager.try :name
+                  td project.project_users.map {|project_user| project_user.user.try :name }.join('<br/>').html_safe
+                  td project.pm_url ? link_to('PM URL', project.pm_url) : ''
+                  td project.project_status.try :name
+                end
+              end
+            end
+          end            
+        end    
+      end
+    end
+    columns do
+      column do
         panel 'Reservated users' do
           table do
             thead do
@@ -51,36 +84,6 @@ ActiveAdmin.register_page "Dashboard" do
           end
         end
       end
-    end
-
-    panel 'Active projects' do
-      table do
-        thead do
-          th do
-            td 'Name'
-            td 'Begin date'
-            td 'End date'
-            td 'Project manager'
-            td 'Members'
-            td 'PM URL'
-            td 'Status'
-          end
-        end
-        tbody do
-          Project.active.each do |project|
-            tr do
-              td
-              td link_to project.name, admin_project_path(project)
-              td l(project.begin_date)
-              td l(project.end_date)                   
-              td project.project_manager.try :name
-              td project.project_users.map {|project_user| project_user.user.try :name }.join(', ')
-              td project.pm_url ? link_to('PM URL', project.pm_url) : ''
-              td project.project_status.try :name
-            end
-          end
-        end
-      end            
     end
     
   end # content
