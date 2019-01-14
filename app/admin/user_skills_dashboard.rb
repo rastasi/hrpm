f7acc64d45ef733810594eb82b3c439f999c69ef
Tasnádi Zsolt <tasi@syrakuza.hu>
2019-01-14 11:35:45 +0100

    user_skills_dashboard add level to summary

--- app/admin/user_skills_dashboard.rb
+++ app/admin/user_skills_dashboard.rb
@@ -16,7 +16,7 @@ ActiveAdmin.register_page "UserSkillsDashboard" do
             tr do
               td
               td user.name
-              td user.user_skills.all.map { |us| us.skill.name }.join('<br />').html_safe
+              td user.user_skills.all.map { |us| "#{us.skill.name} (#{us.level})" }.join('<br />').html_safe
               td do
                 link_to('Edit', admin_user_user_skills_path(user), class: 'member_link')
               end
