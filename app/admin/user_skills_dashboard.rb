bbcd4e1cf8811def7b21df996c8338d80f06075e
Tasnádi Zsolt <tasi@syrakuza.hu>
2019-01-14 11:26:19 +0100

    user_skills_dashboard

---
+++ app/admin/user_skills_dashboard.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register_page "UserSkillsDashboard" do
+
+  menu priority: 1, label: 'User Skills', parent: 'Misc'
+
+  content title: 'User Skills' do
+      table do
+        tbody do
+          thead do
+            th do
+              td 'User'
+              td 'Skills'
+              td 'Opereations'
+            end
+          end
+          User.all.each do |user|
+            tr do
+              td
+              td user.name
+              td user.user_skills.all.map { |us| us.skill.name }.join('<br />').html_safe
+              td do
+                link_to('Edit', admin_user_user_skills_path(user), class: 'member_link')
+              end
+            end
+          end
+        end
+      end
+    end
+end
